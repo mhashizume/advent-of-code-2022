420701e7edc11af5af83090abe0965cbedcc865a
Michael Hashizume <michael.hashizume@puppet.com>
2022-12-02 16:19:10 -0800

    Day 1 part 1

---
+++ 01/calorie.rb
@@ -0,0 +1,15 @@
+def sanitize_input
+  input_file = File.open('./input')
+  input_array = input_file.read.split("\n\n")
+  array_of_arrays = input_array.map {|x| x.split("\n") }
+  int_arrays = array_of_arrays.map {|y| y.map {|z| z.to_i } }
+  int_arrays
+end
+
+def calculate_calories(input)
+  combined_array = []
+  input.each {|array| combined_array.push(array.sum) }
+  puts combined_array.max
+end
+
+calculate_calories(sanitize_input)
