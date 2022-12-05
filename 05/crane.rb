e6fb5309b252f013899a509a0da6937dc5158d92
Michael Hashizume <michael.hashizume@gmail.com>
2022-12-05 12:51:32 -0800

    Day 5 part 1 (crane supply stacks)

--- 05/crane.rb
+++ 05/crane.rb
@@ -1,28 +1,36 @@
-input_file = File.readlines('input')
+input_file = File.readlines('example_input')
 
+array_hash = {}
 
-input_file.scan(/[[:upper:]]/)
+input_file.each do |line|
+  if line.match?(/[A-Z]/)
+    indices = line.enum_for(:scan, /[A-Z]/).map do
+      Regexp.last_match.offset(0).first
+    end
+    adjusted_indices = indices.map { |i| (i + 3) / 4 }
 
-# can't iterate over a string
-# only returns the first match
-y.each {|i| i.index(i.match(/[[:upper:]]/).to_s) }
+    crate_hash = adjusted_indices.zip(line.scan(/[A-Z]/)).to_h
 
-match caps
-count whitespace?
-match 
-
-# count number of arrays
-input_file.each { |line|
-  if line.match?(/^\s\d/)
-    array_numbers = line.strip.split(/\s+/)
-
+    crate_hash.each do |index, crate|
+      if !array_hash["stack_#{index}"]
+        array_hash["stack_#{index}"] = [crate]
+      else
+        array_hash["stack_#{index}"].unshift(crate)
+      end
+    end
   end
-  return
-}
 
+  if line.match?(/^move/)
+    str_qty, src, dest = line.scan(/[0-9]+/)
+    int_qty = str_qty.to_i
 
-# create arrays of each of the columns
-#   gather capital letters in arrays
-#   ensure they're are properly labeled
+    while int_qty.positive? && !array_hash["stack_#{src}"].empty?
+      array_hash["stack_#{dest}"].push(array_hash["stack_#{src}"].pop).flatten!
+      int_qty -= 1
+    end
+  end
+end
 
-# parse the instructions to shift/push to arrays
+array_hash.sort.each do |key, value|
+  puts "#{key}: #{value[-1]}"
+end
