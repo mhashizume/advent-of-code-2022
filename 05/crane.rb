1abfb2ebcce9f7e8f2019e796bcbc27864117758
Michael Hashizume <michael.hashizume@gmail.com>
2022-12-04 21:56:28 -0800

    Day 5 part 1 WIP

---
+++ 05/crane.rb
@@ -0,0 +1,28 @@
+input_file = File.readlines('input')
+
+
+input_file.scan(/[[:upper:]]/)
+
+# can't iterate over a string
+# only returns the first match
+y.each {|i| i.index(i.match(/[[:upper:]]/).to_s) }
+
+match caps
+count whitespace?
+match 
+
+# count number of arrays
+input_file.each { |line|
+  if line.match?(/^\s\d/)
+    array_numbers = line.strip.split(/\s+/)
+
+  end
+  return
+}
+
+
+# create arrays of each of the columns
+#   gather capital letters in arrays
+#   ensure they're are properly labeled
+
+# parse the instructions to shift/push to arrays
