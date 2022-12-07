a00de1547018d34dfd09858e2b2bfc6b3264aaa6
Michael Hashizume <michael.hashizume@gmail.com>
2022-12-06 16:55:57 -0800

    Day 6 part 2 (message marker)

--- 06/packet_marker.rb
+++ 06/packet_marker.rb
@@ -1,12 +1,30 @@
 input_file = File.read('input')
 
-counter = 4
-sequence = input_file.slice!(0, 4).chars
+def part_one(input)
+  part_one_input = input.dup
+  counter = 4
+  sequence = part_one_input.slice!(0, 4).chars
 
-while sequence != sequence.uniq do 
-  sequence.shift
-  sequence.push(input_file.slice!(0, 1))
-  counter += 1
+  while sequence != sequence.uniq do 
+    sequence.shift
+    sequence.push(part_one_input.slice!(0, 1))
+    counter += 1
+  end
+  puts counter
 end
 
-puts counter
+def part_two(input)
+  part_two_input = input.dup
+  counter = 14
+  sequence = part_two_input.slice!(0, 14).chars
+
+  while sequence != sequence.uniq do 
+    sequence.shift
+    sequence.push(part_two_input.slice!(0, 1))
+    counter += 1
+  end
+  puts counter
+end
+
+part_one(input_file)
+part_two(input_file)
